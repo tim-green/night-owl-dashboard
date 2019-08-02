4b7291275e1a9891b4375480cc1b4cc3f61c1713
Tim <github@timgreen.ws>
2019-08-02 17:48:51 +1000

    added watch back to tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,5 +105,5 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default',['concat','uglify','cssmin']); //, 'watch'
+    grunt.registerTask('default',['concat','uglify','cssmin', 'watch']); //, 'watch'
 }
