e7f62b175553371473bcacc07b8358cdd9a106c5
Tim <github@timgreen.ws>
2019-08-12 20:12:45 +1000

    create mapbox init file

---
+++ assets/js/mapbox-init.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JvdW5kY3RybCIsImEiOiJjanhvb2FuczkwOTBxM2RwOWR2M2dzcTBvIn0.4OIjhU9J4sQVJGkNIF1eVg';
+    var map = new mapboxgl.Map({
+    container: 'mapb-general',
+    style: 'mapbox://styles/mapbox/streets-v11'
+    });
